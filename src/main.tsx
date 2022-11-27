b8fcd421d7c22f72ec2a8a14833e38023d95f35b
pshdev1030 <pshdev1030@gmail.com>
2022-11-27 21:05:01 +0900

    🎨:style

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import "./styles/layout.css";
+import "./styles/react.css";
 import "../server";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
